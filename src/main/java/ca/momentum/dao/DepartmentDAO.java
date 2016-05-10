0c845c75a0d151fbc3f6c476cff653464f50f1a0
Thiago Lima <thiago8708@gmail.com>
2016-05-10 16:02:38 -0400

    First commit

---
+++ src/main/java/ca/momentum/dao/DepartmentDAO.java
@@ -0,0 +1,7 @@
+package ca.momentum.dao;
+
+/**
+ * Created by Thiago Lima on 2016-05-10.
+ */
+public class DepartmentDAO {
+}
---
+++ src/main/java/ca/momentum/dao/impl/DepartmentDAOImpl.java
@@ -0,0 +1,7 @@
+package ca.momentum.dao.impl;
+
+/**
+ * Created by Thiago Lima on 2016-05-10.
+ */
+public class DepartmentDAOImpl {
+}
